55ed74153aa2d0f61c99a51fa8afa3881270582a
Adrian Zankich <adrian@hybridgroup.com>
2014-04-16 21:21:30 -0700

    Add analog sensor tests

--- analog_sensor_test.go
+++ analog_sensor_test.go
@@ -19,8 +19,13 @@ var _ = Describe("Analog-Sensor", func() {
 	It("Must be able to Read", func() {
 		Expect(someDriver.Read()).To(Equal(99))
 	})
-
 	It("Must be able to Start", func() {
 		Expect(someDriver.Start()).To(Equal(true))
+	})
+	It("Must be able to Halt", func() {
+		Expect(someDriver.Halt()).To(Equal(true))
+	})
+	It("Must be able to Init", func() {
+		Expect(someDriver.Init()).To(Equal(true))
 	})
 })
--- test_helper.go
+++ test_helper.go
@@ -1,32 +1,15 @@
 package gobotGPIO
 
-type TestAdaptor struct {
-}
+type TestAdaptor struct{}
 
+func (t TestAdaptor) AnalogWrite(string, byte)  {}
+func (t TestAdaptor) DigitalWrite(string, byte) {}
+func (t TestAdaptor) ServoWrite(string, byte)   {}
+func (t TestAdaptor) PwmWrite(string, byte)     {}
+func (t TestAdaptor) InitServo()                {}
 func (t TestAdaptor) AnalogRead(string) int {
 	return 99
 }
-
-func (t TestAdaptor) AnalogWrite(string, byte) {
-	return
-}
-
-func (t TestAdaptor) PwmWrite(string, byte) {
-	return
-}
-
 func (t TestAdaptor) DigitalRead(string) int {
 	return 1
-}
-
-func (t TestAdaptor) DigitalWrite(string, byte) {
-	return
-}
-
-func (t TestAdaptor) InitServo() {
-	return
-}
-
-func (t TestAdaptor) ServoWrite(string, byte) {
-	return
 }
